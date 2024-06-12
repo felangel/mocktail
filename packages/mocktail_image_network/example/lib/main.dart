50330db60db358cffc38b1c1bd7a0314a337e6eb
Felix Angelov <felangelov@gmail.com>
2024-06-11 22:10:11 -0500

    chore(mocktail_image_network): include `pkg:flutter_svg` in example (#240)

--- packages/mocktail_image_network/example/lib/main.dart
+++ packages/mocktail_image_network/example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 /// {@template fake_app}
 /// Sample app used to showcase `mocktail_image_network`
@@ -12,9 +13,17 @@ class FakeApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Image.network(
-            // URL to the Flutter logo from https://flutter.dev/brand
-            'https://storage.googleapis.com/cms-storage-bucket/c823e53b3a1a7b0d36a9.png',
+          child: Column(
+            children: [
+              Image.network(
+                // URL to the png Flutter logo from https://flutter.dev/brand
+                'https://storage.googleapis.com/cms-storage-bucket/c823e53b3a1a7b0d36a9.png',
+              ),
+              SvgPicture.network(
+                // URL to the svg Flutter logo from https://flutter.dev/brand
+                'https://storage.googleapis.com/cms-storage-bucket/847ae81f5430402216fd.svg',
+              ),
+            ],
           ),
         ),
       ),
--- packages/mocktail_image_network/example/test/image_test.dart
+++ packages/mocktail_image_network/example/test/image_test.dart
@@ -1,5 +1,6 @@
 import 'package:example/main.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
 
@@ -7,5 +8,6 @@ void main() {
   testWidgets('can use mocktail for network images', (tester) async {
     await mockNetworkImages(() async => tester.pumpWidget(const FakeApp()));
     expect(find.byType(Image), findsOneWidget);
+    expect(find.byType(SvgPicture), findsOneWidget);
   });
 }
